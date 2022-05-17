2292f8c05f9d8d33cd706eac6fafb4601222d9bf
PralayLahiri <pralaykr.lahiri15@gmail.com>
2022-05-17 21:33:43 +0530

    commit

--- Desktop/django_ajax_mysql/Ajax/Ajax/urls.py
+++ Desktop/django_ajax_mysql/Ajax/Ajax/urls.py
@@ -19,6 +19,10 @@ from enroll import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home, name = 'home'),
+    path('',views.login, name = 'login'),
+    path('/home',views.home, name = 'home'),
+    path('/logout',views.logout, name = 'logout'),
     path('save/',views.save_data, name = 'save'),
+    path('delete/',views.delete_data, name = 'delete'),
+    path('edit/',views.edit_data, name = 'edit'),
 ]
